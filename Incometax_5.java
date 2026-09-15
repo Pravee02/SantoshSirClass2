22bf5e464c9eeaf35ceb2219a7122f3154bd2a06
Praveen M <praveenmpravee7242@gmail.com>
2026-09-15 12:54:04 +0530

    new file

--- Incometax_5.java
+++ Incometax_5.java
@@ -6,11 +6,11 @@ public class Incometax_5 {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter you salary more than 20k");
         int salary = sc.nextInt();
-        int tax = 0;
-        if (salary < 20000) {
+       // int tax = 0;
+        if (salary < 20_000) {
             System.out.println("no income tax ");
         } else {
-            tax = 20 / 1000 * salary;
+           int tax = 20*salary/100;
             System.out.println(tax);
         }
 
