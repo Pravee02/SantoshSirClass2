190b415dad618c9ac3c4b70a6ffbce4e2455642f
Praveen M <praveenmpravee7242@gmail.com>
2026-09-15 13:05:35 +0530

    new file

---
+++ Vowel_6.java
@@ -0,0 +1,17 @@
+import java.util.*;;
+
+public class Vowel_6 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a character to check vowel or not");
+        char letter = (char) sc.next().charAt(0);
+
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+            System.out.println("Ovewl");
+        } else {
+            System.out.println("not ovewl");
+        }
+    }
+}
