f3eb03c302c07c7ff0940c6487f623b6b65742a4
Praveen M <praveenmpravee7242@gmail.com>
2026-09-16 20:21:01 +0530

    new file

---
+++ Ascii_7.java
@@ -0,0 +1,8 @@
+public class Ascii_7 {
+    public static void main(String[] args) {
+
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.println(ch + " " + (int) ch);
+        }
+    }
+}
--- Vowel_6.java
+++ Vowel_6.java
@@ -6,14 +6,14 @@ public class Vowel_6 {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a character to check vowel or not");
-        char letter = (char) sc.next().charAt(0);
+        char letter = sc.next().charAt(0);
 
         if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
             System.out.println("Ovewl");
         } else if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U') {
             System.out.println("Ovewl");
         } else {
-            System.out.println(" Consonant ");
+            System.out.println("Consonant ");
         }
     }
 }
