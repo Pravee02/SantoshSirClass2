6fb164630f4bba1a097691e1b1cd7d0104fb216c
Praveen M <praveenmpravee7242@gmail.com>
2026-09-23 12:02:35 +0530

    new file

--- VarArgs.java
+++ VarArgs.java
@@ -1,26 +1,36 @@
 public class VarArgs
 {
-    static void show()
-    {
-        System.out.println("No Args");
-    }
+    // static void show()
+    // {
+    //     System.out.println("No Args");
+    // }
 
-    static void show(int a)
-    {
-        System.out.println("one arg");
-    }
+    // static void show(int a)
+    // {
+    //     System.out.println("one arg");
+    // }
 
-    static void show(int a , int b)
+    // static void show(int a , int b)
+    // {
+    //     System.out.println("Two args");
+    // }
+
+    static void show(int...a)
     {
-        System.out.println("Two args");
+        System.out.println("Arguments");
+        for(int i = 0 ; i < a.length ; i++)
+        {
+            System.out.println(a[i]);
+        }
     }
 
     public static void main(String[] args)
     {
     
-        show();
+      //  show();
         show(15);
         show(15, 16);
+        show(15,16,17,18,19); //show(new int[]{15,16,17,18,19})
 
     }
 }
